4396ec0ebf20dc51e70fcaae2e9347245feb5b63
Michael Cheng <michael_cheng@tech.gov.sg>
2025-04-19 13:02:46 +0800

    Update meta data
    Co-authored-by: Tai Shi Ling <shiling.tai@gmail.com>
    Co-authored-by: Chay Choong <chaychoong@gmail.com>

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,16 +1,16 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_TITLE, SITE_DESCRIPTION } from "../consts";
 
 export async function GET(context) {
-	const posts = await getCollection('blog');
-	return rss({
-		title: SITE_TITLE,
-		description: SITE_DESCRIPTION,
-		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.id}/`,
-		})),
-	});
+  const events = await getCollection("events");
+  return rss({
+    title: SITE_TITLE,
+    description: SITE_DESCRIPTION,
+    site: context.site,
+    items: events.map((event) => ({
+      ...event.data,
+      link: `/events/${event.id}/`,
+    })),
+  });
 }
